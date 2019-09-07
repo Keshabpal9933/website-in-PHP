aac41549e2fe4d3012eac19121ba035553a4ba43
Keshab Pal <keshabpal9933@gmail.com>
2019-09-07 13:14:13 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>Herp-Technology</title>
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <link rel="icon" href="logo.jpg">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <div class="bgimg">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+      <div class="container">
+        <a href="" class="navbar-brand text-warning font-weight-bold"><h3>Herp-Technology</h3>
+        </a>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsenavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+         <div class="collapse navbar-collapse text-center"  id="collapsenavbar">
+           
+           <ul class="navbar-nav ml-auto">
+
+            <li class="nav-itam">
+              <a href="" class="nav-link text-white"><strong>Services</strong></a>  
+            </li>
+
+            <li class="nav-itam">
+              <a href="" class="nav-link text-white"><strong>Portfolio</strong></a>  
+            </li>
+
+            <li class="nav-itam">
+              <a href="" class="nav-link text-white"><strong>About</strong></a>  
+            </li>
+
+            <li class="nav-itam">
+              <a href="" class="nav-link text-white"><strong>Our-Team</strong></a>  
+            </li>
+
+            <li class="nav-itam">
+              <a href="" class="nav-link text-white"><strong>Contact</strong></a>  
+            </li>
+             
+           </ul>
+         </div>
+
+      </div>
+    </nav>
+    <div class="container text-center hearderset text-danger">
+      <h2>Welcome To Herp-Technology</h2>
+      <h1>Visit our Website Scroll Down</h1>
+     <button class="btn btn-success text-white"><h2>Welcom To Herp-Technology</h2></button>
+    </div>
+
+  </div>
+  <!----------------our services blogs--------------->
+  <section class="container ourservices text-center">
+    <h1>Services</h1>
+    <p>We provide quality services to our customers</p>
+    <div class="row rowsetting">
+      <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+        <div class="imgsetting d-block m-auto bg-warning"><i class="fa fas fa-cart-arrow-down fa-4x"></i>
+          <h2>E-Commerce</h2>
+          <p>E-commerce (electronic commerce) is the buying and selling of goods and services, or the transmitting of funds or data, over an electronic network, primarily the internet.</p>
+          
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+        <div class="imgsetting d-block m-auto bg-warning"><i class="fa fas fa-desktop fa-4x"></i>
+          <h2>E-Banking</h2>
+          <p>Online banking, also known as internet banking, is an electronic payment system that enables customers of a bank or other financial institution to conduct a range of financial transactions through the financial institution's website.</p>
+          
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4 col-10 d-block m-auto">
+        <div class="imgsetting d-block m-auto bg-warning"><i class="fa fas fa-briefcase fa-4x"></i>
+          <h2>E-Business</h2>
+          <p>Online Business or e-business is any kind of business or commercial transaction that includes sharing information across the internet. The exchange of products and services between businesses, groups and individuals of any business.</p>
+          
+        </div>
+      </div>
+
+    </div>
+    <!-- portfolio section-->
+  </section>
+  <section class="portfolio ">
+    <div class="container text-center">
+      <h1>Portfolio</h1>
+      <p>We provide quality services to our customers</p>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="sh.jpg" class="card-img img-fluid">
+            <div class="card-body">
+              <h2 class="card-title">Shirts</h2>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="gl.jpg" class="card-img img-fluid">
+            <div class="card-body">
+              <h2 class="card-title">Girl's</h2>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="np.jpg" class="card-img img-fluid">
+            <div class="card-body">
+              <h2 class="card-title">Jersey</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="lp.png" class="card-img img-fluid img-responsive height=250px width=250px">
+            <div class="card-body">
+              <h2 class="card-title">Laptop's</h2>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="mbl.jpg" class="card-img img-fluid height=250px width=250px">
+            <div class="card-body">
+              <h2 class="card-title">Mobile's</h2>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="fn.jpg" class="card-img img-fluid height=250px width=250px">
+            <div class="card-body">
+              <h2 class="card-title">Fan's</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="gf.jpg" class="card-img img-fluid height=250px width=250px">
+            <div class="card-body">
+              <h2 class="card-title">Gift's</h2>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="al.jpg" class="card-img img-fluid height=250px width=250px">
+            <div class="card-body">
+              <h2 class="card-title">Alcohol's</h2>
+            </div>
+          </div>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <div class="card">
+            <img src="cl.png" class="card-img img-fluid height=250px width=250px">
+            <div class="card-body">
+              <h2 class="card-title">Cold-Drink's</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--our amazing team ---->
+  <section class="ourteam">
+    <div class="container text-center">
+      <h1>Our Amazing Team</h1>
+      <p>Our Team provide best quality services</p>
+      <div class="row teamsetting">
+        <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <figure class="figure">
+            <img src="me.jpg" class="figure-img img-fluid rounded-circle" height="200px" width="200px">
+            <figcaption>
+              <h3>Keshab Pal</h3>
+              <p class="figure-caption">Web-Developer</p>
+            </figcaption>
+          </figure>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <figure class="figure">
+            <img src="puskar.jpg" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+            <figcaption>
+              <h3>Puskar Bist</h3>
+              <p class="figure-caption">CEO</p>
+            </figcaption>
+          </figure>
+        </div>
+         <div class="col-lg-4 col-md-4 col-sm-12 col-10 d-block m-auto">
+          <figure class="figure">
+            <img src="bd.jpg" class="figure-img img-fluid rounded-circle" height="250px" width="250px">
+            <figcaption>
+              <h3>Binod Pant</h3>
+              <p class="figure-caption">Java-Developer</p>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+
+    </div>
+    
+  </section>
+  <!------ footer begin------>
+  <footer class="footer">
+    <h5 class="text-center"> copyright © 2019 Developed By Keshab Pal</h5>
+  </footer>
+</body>
+</html>
